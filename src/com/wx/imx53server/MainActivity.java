0b1e5e84cb4d50db69a38e151fed225e0f8ee1e2
WXrock <wxedmend@gmail.com>
2015-06-26 22:12:30 +0800

    add network function,still some bugs

--- src/com/wx/imx53server/MainActivity.java
+++ src/com/wx/imx53server/MainActivity.java
@@ -2,6 +2,8 @@ package com.wx.imx53server;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
@@ -24,7 +26,8 @@ public class MainActivity extends Activity {
 	private MyCamera mCamera;
 	private StringBuilder str;
 	
-	private Thread server = null;
+	private ServerThread server = null;
+	private Handler mServerHandler = null;
 	
 	private static final String TAG = "MainActivity";
 
@@ -43,48 +46,37 @@ public class MainActivity extends Activity {
 		this.str = new StringBuilder(camInfo.getText());
 		this.mCamera = new MyCamera(mSurfaceView);
 		this.mCamera.setNum(0);
-		
+		this.mServerHandler = new Handler(){
+
+			@Override
+			public void handleMessage(Message msg) {
+				String str = String.valueOf(msg.obj);
+				if(str.equals("change")){
+					Log.d(TAG,str);
+					changeCamFun();
+				}else if(str.equals("pic")){
+					Log.d(TAG,str);
+					takePicFun();
+				}else if(str.equals("preview")){
+					Log.d(TAG,str);
+					previewFun();
+				}
+			}
+			
+		};
+				
 		mPreviewBut.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				if(mCamera.isPriv()){
-					mCamera.stopPreview();
-					mPreviewBut.setText("start preview");
-				}else{
-					mCamera.startPreview();
-					mPreviewBut.setText("stop preview");
-				}
-				
+				previewFun();			
 			}
 		});
 		
 		mPictureBut.setOnClickListener(new OnClickListener() {
-			private String path;
 			@Override
 			public void onClick(View v) {
-				if(mCamera.getMode() == 0){ //use android's take picture method
-					if(mCamera.isPriv()){
-
-						path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/test"+mCamera.getNum()+".jpg";
-						mCamera.shootAt(path);
-					}else{
-						Toast.makeText(MainActivity.this, "start prewview before take a picture", Toast.LENGTH_SHORT).show();
-					}
-				}else{          //use jni take picture method
-					mCamera.stopPreview();
-					mCamera.release();
-					if(mCamera.takePicture() <0){
-						Toast.makeText(MainActivity.this, "take picture error", Toast.LENGTH_SHORT).show();
-					}else{
-						Toast.makeText(MainActivity.this, "take picture success", Toast.LENGTH_SHORT).show();
-					}
-					mCamera.setMode(0);
-					mCamera.prepareAndroidCamera(mCamera.getHolder());
-					mCamera.startPreview();
-				}
-				
-				
+				takePicFun();								
 			}
 		});
 		
@@ -92,13 +84,7 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				if(mCamera.changeCam() < 0){
-					Log.e(TAG,"write gpio failed");
-				}else{
-					MainActivity.this.str.replace(str.length()-1, str.length(), 
-							String.valueOf(MainActivity.this.mCamera.getNum()));
-					MainActivity.this.camInfo.setText(MainActivity.this.str.toString());
-				}		
+				changeCamFun();
 			}
 		});
 		
@@ -119,22 +105,63 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-		this.server = new Thread(new ServerThread());
+		//this.server = new Thread(new ServerThread(mServerHandler));
+		this.server = new ServerThread(mServerHandler);
 		this.server.start();
 		
 	}
 
+	
+	private void changeCamFun(){
+		if(mCamera.changeCam() < 0){
+			Log.e(TAG,"write gpio failed");
+		}else{
+			MainActivity.this.str.replace(str.length()-1, str.length(), 
+					String.valueOf(MainActivity.this.mCamera.getNum()));
+			MainActivity.this.camInfo.setText(MainActivity.this.str.toString());
+		}	
+	}
+	
+	private void takePicFun(){
+		if(mCamera.getMode() == 0){ //use android's take picture method
+			if(mCamera.isPriv()){
 
+				String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/test"+mCamera.getNum()+".jpg";
+				mCamera.shootAt(path);
+			}else{
+				Toast.makeText(MainActivity.this, "start prewview before take a picture", Toast.LENGTH_SHORT).show();
+			}
+		}else{          //use jni take picture method
+			mCamera.stopPreview();
+			mCamera.release();
+			if(mCamera.takePicture() <0){
+				Toast.makeText(MainActivity.this, "take picture error", Toast.LENGTH_SHORT).show();
+			}else{
+				Toast.makeText(MainActivity.this, "take picture success", Toast.LENGTH_SHORT).show();
+			}
+			mCamera.setMode(0);
+			mCamera.prepareAndroidCamera(mCamera.getHolder());
+			mCamera.startPreview();
+		}
+	}
+	
+	private void previewFun(){
+		if(mCamera.isPriv()){
+			mCamera.stopPreview();
+			mPreviewBut.setText("start preview");
+		}else{
+			mCamera.startPreview();
+			mPreviewBut.setText("stop preview");
+		}
+	}
+	
 	@Override
 	protected void onDestroy() {
 		mCamera.setMode(0);
 		mCamera.clearNum();
+		this.server.release();
 		super.onDestroy();
 	}
-	
-	
-	
-	
 
 
 }
--- src/com/wx/imx53server/ServerThread.java
+++ src/com/wx/imx53server/ServerThread.java
@@ -6,56 +6,96 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
-public class ServerThread implements Runnable{
+public class ServerThread extends Thread implements Runnable {
 	
 	public static final String TAG = "ServerThrread";
     private Socket client = null;
-    private String str ;
+//    private String str ;
     private ServerSocket server = null;
     private BufferedReader buf = null;
     private PrintStream out = null;
     public boolean bye = false;
-    public boolean isConnected = false;
+    private boolean isConnected = false;
+    private boolean isStoped = false;
     
-    public void run(){
+    private Handler mHandler = null; 
+    private Message mMessage = null;
+    
+    
+    public ServerThread(Handler handler) {
+    	this.mHandler = handler;
+    	this.mMessage = new Message();
     	try {
 			this.server = new ServerSocket(8888);
-			this.client = server.accept();
-			this.isConnected = true;
-		} catch (IOException e1) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e.printStackTrace();
 		}
-    	
-
-        try{
-             out = new PrintStream(client.getOutputStream());
-             buf = new BufferedReader(new InputStreamReader(client.getInputStream()));
-             while(!bye){
-                str = buf.readLine();
-                if(str.equals("bye")){
-                	bye = true;
-                }else if(str.equals("change")){
-                	Log.d(TAG,"change");
-                }else if(str.equals("pic")){
-                	Log.d(TAG,"pic");
-                }else if(str.equals("preview")){
-                	Log.d(TAG,"preview");
-                }else{
-                	
-                }
-             }
-             client.close();
-             this.isConnected = false;
-        }catch(Exception e){
-        	e.printStackTrace();
+    }
+    
+    
+    @Override
+    public void run(){
+		if(this.server == null){
+			Log.e(TAG,"server is null");
+			this.isStoped = true;
+		}
+    	while(!isStoped){
+    		try {   				
+    			this.client = server.accept();
+    			if(client.isConnected()){
+    				Log.d(TAG,"client connected!");    	
+    				this.isConnected = true;
+    				bye = false;
+    			}
+    		} catch (IOException e1) {
+    			// TODO Auto-generated catch block
+    			e1.printStackTrace();
+    		}   	
+    		try{
+    			out = new PrintStream(client.getOutputStream());
+    			buf = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    			while(!bye){
+    				String str = buf.readLine();
+    				if(str == null || str.equals("") || str.equals("bye")){
+    					bye = true;
+    					Log.d(TAG,"client disconnect!!");
+    				}else{
+    					mMessage.obj = str;
+    					Log.d(TAG,str);
+    					mHandler.sendMessage(mMessage);
+    				}
+    			}
+    			client.close();
+    			this.isConnected = false;
+    		}catch(Exception e){
+    			e.printStackTrace();
+    		}
         }
     }
     
     public boolean isConnect(){
     	return this.isConnected;
+    }
+    
+    public void release(){
+    	try {
+			server.close();   //it will cost some time for the system to release port;
+			client.close();
+			server = null;
+			client = null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	this.bye = true;
+    	this.isConnected = false;
+    	this.isStoped = true;
     }
 
 }
